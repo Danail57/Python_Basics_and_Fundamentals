fd1fb297d89f195b3e4bf7c437f5229f4c6c0e8c
Danail Donkov <danail.donkov5@gmail.com>
2026-01-15 00:55:05 +0200

    Create Number_Filter.py

---
+++ Lists/Number_Filter.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+numbers = []
+for _ in range(n):
+    current_number = int(input())
+    numbers.append(current_number)
+
+command = input()
+
+filtered_numbers = []
+
+for number in numbers:
+    if command == "even" and number % 2 == 0:
+        filtered_numbers.append(number)
+    elif command == "odd" and number % 2 != 0:
+        filtered_numbers.append(number)
+    elif command == "positive" and number >= 0:
+        filtered_numbers.append(number)
+    elif command == "negative" and number < 0:
+        filtered_numbers.append(number)
+
+print(filtered_numbers)
