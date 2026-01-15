912da8d1fee072c89c70bc840c591f73b41560ed
Danail Donkov <danail.donkov5@gmail.com>
2026-01-16 00:04:25 +0200

    Create The_Smallest_Number.py

---
+++ Functions_in_Python/The_Smallest_Number.py
@@ -0,0 +1,9 @@
+def the_smallest_number():
+    number_list = [first_number, second_number, third_number]
+    return min(number_list)
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+the_smallest_number()
+print(the_smallest_number())
